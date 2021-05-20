e452bedecb0f13e180ba93d4db84c88727407494
Vas Zhigalov <vas.zhigalov@gmail.com>
2021-05-20 22:02:22 +0300

    Ajax add to cart for single product

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -298,6 +298,41 @@ if ( ! function_exists( 'imicra_woocommerce_cart_link' ) ) {
 	}
 }
 
+/**
+ * Ajax add to cart for single product
+ */
+function imicra_ajax_add_to_cart_cb() {
+	$product_id        = apply_filters( 'woocommerce_add_to_cart_product_id', absint( $_POST['product_id'] ) );
+	$quantity          = empty( $_POST['quantity'] ) ? 1 : wc_stock_amount( wp_unslash( $_POST['quantity'] ) );
+	$variation_id      = absint( $_POST['variation_id'] );
+	$passed_validation = apply_filters( 'woocommerce_add_to_cart_validation', true, $product_id, $quantity );
+	$product_status    = get_post_status( $product_id );
+
+	if ( $passed_validation && WC()->cart->add_to_cart($product_id, $quantity, $variation_id) && 'publish' === $product_status ) {
+		
+		do_action( 'woocommerce_ajax_added_to_cart', $product_id );
+
+		if ( 'yes' === get_option( 'woocommerce_cart_redirect_after_add' ) ) {
+			wc_add_to_cart_message( array( $product_id => $quantity ), true );
+		}
+
+		WC_AJAX :: get_refreshed_fragments();
+
+	} else {
+
+		$data = array(
+			'error'       => true,
+			'product_url' => apply_filters( 'woocommerce_cart_redirect_after_error', get_permalink( $product_id ), $product_id ),
+		);
+
+		echo wp_send_json( $data );
+	}
+
+	wp_die();
+}
+add_action( 'wp_ajax_imicra_ajax_add_to_cart', 'imicra_ajax_add_to_cart_cb' );
+add_action( 'wp_ajax_nopriv_imicra_ajax_add_to_cart', 'imicra_ajax_add_to_cart_cb' );
+
 if ( ! function_exists( 'imicra_woocommerce_header_cart' ) ) {
 	/**
 	 * Display Header Cart.
